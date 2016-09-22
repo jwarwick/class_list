d2280da36d30d01abdaef4fe78eff650e3f8a49b
John Warwick <jwarwick@gmail.com>
2016-09-22 15:57:57 -0400

    Better printing of addresses in the directory rtf

--- lib/class_list/display.ex
+++ lib/class_list/display.ex
@@ -39,4 +39,34 @@ defmodule ClassList.Display do
     end
   end
 
+  @doc """
+  Format the city/state/zip line of the address
+  """
+  def format_city_address(city, state, zip) do
+    {has_city, city} = fix_string(city)
+    {has_state, state} = fix_string(state)
+    state = format_state(state)
+    {_has_zip, zip} = fix_string(zip)
+    
+    comma = if (has_city && has_state) do
+      ", "
+    else
+      ""
+    end
+
+    "#{city}#{comma}#{state}  #{zip}"
+  end
+
+  # returns {string_has_content_boolean?, string_value ("" if has_content is false}
+  defp fix_string(nil), do: {false, ""}
+  defp fix_string(str) do
+    str = String.trim(str)
+    {0 != String.length(str), str}
+  end
+
+  # defp not_empty(nil), do: false
+  # defp not_empty(str) do
+  #   length = str |> String.trim |> String.length
+  #   0 != length
+  # end
 end
